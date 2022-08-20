23a2cb79383176987e9c279de3e2dd598537e4cb
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-08-20 21:50:36 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by 春田実利 on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
